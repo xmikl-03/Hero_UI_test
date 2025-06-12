af654e19b097ed61c171eb8938762d286cbab523
xmikl-03 <148964584+xmikl-03@users.noreply.github.com>
2025-06-12 12:12:53 +0200

    Update DataPanel.tsx

--- src/components/DataPanel.tsx
+++ src/components/DataPanel.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import { Modal, Tabs, Tab } from '@heroui/react';
+import ChartDataTable from './ChartDataTable';
+
 interface Props {
   isOpen: boolean;
   onClose: () => void;
@@ -5,17 +9,22 @@ interface Props {
 
 const DataPanel: React.FC<Props> = ({ isOpen, onClose }) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} title="Chart Data Editor">
-      <Tabs>
-        <Tab title="Customer Segmentation">
-          <ChartDataTable chartKey="customerSegmentation" />
-        </Tab>
-        <Tab title="Deal Funnel">
-          <ChartDataTable chartKey="dealFunnel" />
-        </Tab>
-        {/* Add other chart editors here */}
-      </Tabs>
+    <Modal isOpen={isOpen} onClose={onClose} title="Edit Chart Data" size="xl">
+      <div className="p-4">
+        <Tabs>
+          <Tab title="Customer Segmentation">
+            <ChartDataTable chartKey="customerSegmentation" />
+          </Tab>
+          <Tab title="Deal Funnel">
+            <ChartDataTable chartKey="dealFunnel" />
+          </Tab>
+          <Tab title="Revenue by Product">
+            <ChartDataTable chartKey="revenueByProduct" />
+          </Tab>
+        </Tabs>
+      </div>
     </Modal>
   );
 };
 
+export default DataPanel;
